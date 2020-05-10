64970a9d9c9950b8fc2ef711497a2925f55f5245
maheshrapelli141 <maheshrapelli141@gmail.com>
2020-05-10 13:16:56 +0530

    findmore page ui

--- src/pages/doctorsAppointment/findMore/findMore.js
+++ src/pages/doctorsAppointment/findMore/findMore.js
@@ -1,5 +1,5 @@
 import React,{ Component} from 'react'
-import {Card, Col, Row, InputGroup, Formcontrol, Media,FormControl} from 'react-bootstrap'
+import {Card, Col, Row, InputGroup, Button, Media,FormControl} from 'react-bootstrap'
 import {connect} from 'react-redux'
 
 class findMore extends Component{
@@ -15,85 +15,77 @@ class findMore extends Component{
                     </Col>
                 </Row>
                 <Row>
-            <Col>
-              <Card style={{ width: '20rem' }}>
-                <Card.Img variant="top" src="images/page.jpg" />
-                <Card.Body>
-                <Row>
-                   <Col xs lg="3">
-                     <Media>
-                       <img 
-                       width={40}
-                       height={40}
-                       src="images/page.jpg"
-                       />
-                     </Media>
-                   </Col>
-                   <Col xs lg="9">
-                    <InputGroup>
-                      {/* <InputGroup.Prepend>
-                        <InputGroup.Text>With textarea</InputGroup.Text>
-                      </InputGroup.Prepend> */}
-                      <FormControl as="textarea" aria-label="With textarea" />
-                    </InputGroup>
-                   </Col>
-                 </Row>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card style={{ width: '20rem' }}>
-                <Card.Img variant="top" src="images/page.jpg" />
-                <Card.Body>
-                 <Row>
-                   <Col xs lg="3">
-                     <Media>
-                       <img 
-                       width={40}
-                       height={40}
-                       src="images/page.jpg"
-                       />
-                     </Media>
-                   </Col>
-                   <Col xs lg="9">
-                    <InputGroup>
-                      {/* <InputGroup.Prepend>
-                        <InputGroup.Text>With textarea</InputGroup.Text>
-                      </InputGroup.Prepend> */}
-                      <FormControl as="textarea" aria-label="With textarea" />
-                    </InputGroup>
-                   </Col>
-                 </Row>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card style={{ width: '20rem' }}>
-                <Card.Img variant="top" src="images/page.jpg" />
-                <Card.Body>
-                <Row>
-                   <Col xs lg="3">
-                     <Media>
-                       <img 
-                       width={40}
-                       height={40}
-                       src="images/page.jpg"
-                       />
-                     </Media>
-                   </Col>
-                   <Col xs lg="9">
-                    <InputGroup>
-                      {/* <InputGroup.Prepend>
-                        <InputGroup.Text>With textarea</InputGroup.Text>
-                      </InputGroup.Prepend> */}
-                      <FormControl as="textarea" aria-label="With textarea" />
-                    </InputGroup>
-                   </Col>
-                 </Row>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row><br/><br/>
+                  <Col className="docdetail">
+                    <Row>
+                      <Col xl="3" lg="3">
+                        hello
+                      </Col>
+                      <Col xl="9" lg="9">
+                        hello
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col className="docdetail">
+                    <Row>
+                      <Col xl="3" lg="3">
+                        <Media>
+                          <img src="images/page.jpg" className="docprofile" />
+                        </Media>
+                      </Col>
+                      <Col xl="9" lg="9">
+                        <h2>Name</h2>
+                        <p>1 Doctor</p>
+                        <p>ES 500 onwards</p>
+                        <p>500 Feedbacks</p>
+                        <Row>
+                          <Col>
+                            <Media>
+                              <img 
+                                src="images/page.jpg"
+                                width={64}
+                                height={64}
+                                className="mr-3"
+
+                              />
+                            </Media>
+                          </Col>
+                          <Col>
+                            <Media>
+                              <img 
+                                src="images/page.jpg"
+                                width={64}
+                                height={64}
+                                className="mr-3"
+                              />
+                            </Media>
+                          </Col>
+                          <Col>
+                            <Media>
+                              <img 
+                                src="images/page.jpg"
+                                width={64}
+                                height={64}
+                                className="mr-3"
+                              />
+                            </Media>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Button>Treatment</Button>
+                          <Button>Treatment</Button>
+                          <Button>Treatment</Button>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col className="docdetail">
+                    <Row>
+                      <Col xl="3" lg="3"></Col>
+                      <Col xl="9" lg="9"></Col>
+                    </Row>
+                  </Col>
+                </Row>
+               <br/><br/>
             </div>
         )
     }
